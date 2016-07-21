7d85b6066964d6b1956b76c16c10c0858b23580d
AsimoLoveGym <xiazhuangz@gmail.com>
2016-07-21 07:53:57 -0500

    Finished the majority part of work

--- asimo.js
+++ asimo.js
@@ -1,6 +1,9 @@
 // Friends Clicker
+// 这个project不简单的，涉及到使用闭包来实现不同作用域下面的数据传递
 // ************Model*************
 var model = {
+  // currentCat是作为一种buffer一样，或者intermediate一样的存在，传递转用物质。
+  currentCat: null,
   cats: [{
     clickCount: 0,
     name: "Ross",
@@ -38,14 +41,52 @@ var model = {
 
 var octopus ={
   init: function(){
+    // need to init the value for currentCat, unlease cant set name, clickcout, and imgSrc
+    model.currentCat = model.cats[0];
+
     catListView.init();
+    catView.init();
   },
   getCats: function(){
     return model.cats;
+  },
+  setCurrentCat: function(cat){
+    model.currentCat = cat;
+  },
+  incrementCounter: function(){
+    // console.log("increment works");
+    model.currentCat.clickCount++;
+    catView.render();
+  },
+  getCurrentCat: function(){
+    return model.currentCat;
   }
 };
 
 // ************View*************
+var catView = {
+  init: function(){
+    this.catElem = document.getElementById("cat");
+    this.catNameElem = document.getElementById("cat-name");
+    this.countElem = document.getElementById("cat-count");
+    this.catImageElem = document.getElementById("cat-img");
+
+    this.catImageElem.addEventListener("click",function(){
+      octopus.incrementCounter();
+    });
+
+    this.render();
+  },
+  render: function(){
+    var currentCat = octopus.getCurrentCat();
+    // console.log(currentCat);
+    // console.log(currentCat.name);
+    this.catNameElem.textContent = currentCat.name;
+    this.countElem.textContent = currentCat.clickCount;
+    this.catImageElem.src = currentCat.imgSrc;
+  }
+};
+
 
 var catListView = {
   init: function(){
@@ -66,6 +107,15 @@ var catListView = {
       elem = document.createElement('li');
       // var catNameList = document.createElement('li');
       elem.textContent = cat.name;
+
+      // 闭包的关键出现在了这里！！
+      elem.addEventListener("click",(function(catCopy) {
+        return function(){
+          octopus.setCurrentCat(catCopy);
+          catView.render();
+        };
+      })(cat));
+
       this.catListElem.appendChild(elem);
     }
   }
